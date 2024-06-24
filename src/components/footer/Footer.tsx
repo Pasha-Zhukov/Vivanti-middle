10d707b6d02beef399f8edc8bdd3381d266f1494
Pavel Zhukov <pavel.zhukov@erp-flowers.ru>
2024-06-24 09:33:31 +0300

    fix date

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -6,9 +6,9 @@ export default function Footer() {
       <div className="mb-3 mt-3">СВЯЖИТЕСЬ С НАМИ</div>
       <p className="phone">Телефон</p>
       <p className="location mb-5">Адрес</p>
-      <p className="copyright">© 2023 site.RU</p>
+      <p className="copyright">© 2024 site.RU</p>
       <p>Все права защищены</p>
-      <p className="fs-13 mb-0">Дата последнего обновления сайта: 07.02.2023</p>
+      <p className="fs-13 mb-0">Дата последнего обновления сайта: 24.06.2024</p>
     </footer>
   );
 }
